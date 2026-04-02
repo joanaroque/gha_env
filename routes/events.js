245443d3a515711d77ed9aafc82fe6bf31929610
Joana Roque (CTW) <joana.roque@ctw.bmwgroup.com>
2026-04-02 12:17:08 +0100

    try to fix path to database connection

--- routes/events.js
+++ routes/events.js
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 
-import db from '../data/database.js';
+import { getDb } from '../data/database.js';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
+  const db = getDb();
   const allEvents = await db.collection('events').find().toArray();
   res.json({ events: allEvents });
 });
 
 router.post('/', async (req, res) => {
   const eventData = req.body;
+  const db = getDb();
   const result = await db.collection('events').insertOne({...eventData});
   res.status(201).json({
     message: 'Event created.',
